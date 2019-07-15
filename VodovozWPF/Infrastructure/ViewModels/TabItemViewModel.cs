dbec7ed2cc90cebfe0f0b67aa2be6f0b57cd656a
Vadim Gordeev <Enzogord@gmail.com>
2019-07-15 13:29:45 +0300

    Добавлена базовая реализация главного окна

---
+++ VodovozWPF/Infrastructure/ViewModels/TabItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using QS.DomainModel.Entity;
+using QS.ViewModels;
+
+namespace VodovozWPF.Infrastructure.ViewModels
+{
+    public class TabItemViewModel : PropertyChangedBase
+    {  
+        private string header;
+        public string Header {
+            get => header;
+            set => SetField(ref header, value, () => Header);
+        }
+    }
+}
---
+++ VodovozWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using QS.DomainModel.Entity;
+using VodovozWPF.Infrastructure.ViewModels;
+using VodovozWPF.Properties;
+
+namespace VodovozWPF.ViewModels
+{
+    public class MainWindowViewModel : PropertyChangedBase
+    {
+        private ObservableCollection<TabItemViewModel> tabs;
+        public ObservableCollection<TabItemViewModel> Tabs {
+            get => tabs;
+            set => SetField(ref tabs, value, () => Tabs);
+        }
+
+        public MainWindowViewModel()
+        {
+            Tabs = new ObservableCollection<TabItemViewModel>();
+            Tabs.Add(new TabItemViewModel() { Header = "Заказы",        Image = Resources.orders        });
+            Tabs.Add(new TabItemViewModel() { Header = "Логистика",     Image = Resources.logistic      });
+            Tabs.Add(new TabItemViewModel() { Header = "Склад",         Image = Resources.stock         });
+            Tabs.Add(new TabItemViewModel() { Header = "Касса",         Image = Resources.cash          });
+            Tabs.Add(new TabItemViewModel() { Header = "Бухгалтерия",   Image = Resources.accounting    });
+            Tabs.Add(new TabItemViewModel() { Header = "Архив",         Image = Resources.archive       });
+            Tabs.Add(new TabItemViewModel() { Header = "Работа с кл.",  Image = Resources.crm           });
+        }
+    }
+}
